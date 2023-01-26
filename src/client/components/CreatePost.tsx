795aaa718a51ccb4911a309be094043745c3d35a
BrutchsamaJeanLouis <73585384+BrutchsamaJeanLouis@users.noreply.github.com>
2023-01-26 10:18:58 +0000

    kill-ports, debug script

--- src/client/components/CreatePost.tsx
+++ src/client/components/CreatePost.tsx
@@ -1,8 +1,66 @@
+import { Formik } from "formik";
 import React from "react";
 import { connect } from "react-redux";
 
 export const CreatePost = props => {
-  return <div>CreatePost</div>;
+  return (
+    <Formik
+      initialValues={{ email: "", password: "" }}
+      validate={values => {
+        const errors = {};
+        if (!values.email) {
+          errors.email = "Required";
+        } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)) {
+          errors.email = "Invalid email address";
+        }
+        return errors;
+      }}
+      onSubmit={(values, { setSubmitting }) => {
+        setTimeout(() => {
+          alert(JSON.stringify(values, null, 2));
+          setSubmitting(false);
+        }, 400);
+      }}
+    >
+      {({
+        values,
+        errors,
+        touched,
+        handleChange,
+        handleBlur,
+        handleSubmit,
+        isSubmitting
+        /* and other goodies */
+      }) => (
+        <form>
+          <div className="mb-3 w-auto ">
+            <label htmlFor="exampleInputEmail1" className="form-label">
+              Email address
+            </label>
+            <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
+            <div id="emailHelp" className="form-text">
+              {"We'll"} never share your email with anyone else.
+            </div>
+          </div>
+          <div className="mb-3">
+            <label htmlFor="exampleInputPassword1" className="form-label">
+              Password
+            </label>
+            <input type="password" className="form-control" id="exampleInputPassword1" />
+          </div>
+          <div className="mb-3 form-check">
+            <input type="checkbox" className="form-check-input" id="exampleCheck1" />
+            <label className="form-check-label" htmlFor="exampleCheck1">
+              Check me out
+            </label>
+          </div>
+          <button type="submit" className="btn btn-primary">
+            Submit
+          </button>
+        </form>
+      )}
+    </Formik>
+  );
 };
 
 export default connect()(CreatePost);
---
+++ src/utils/validation-schemas/schema-create-post.ts
@@ -0,0 +1,14 @@
+import * as yup from "yup";
+import _ from "lodash";
+
+export const createPostBodySchema: yup.AnyObjectSchema = yup.object({
+  title: yup.string().min(1).max(255).required(),
+  description: yup.string().min(1).required(),
+  tags: yup.array().of(yup.string()).max(5)
+});
+
+export const createPostRequestSchema = yup.object({
+  body: createPostBodySchema,
+  params: yup.object().shape({}).noUnknown(),
+  query: yup.object().shape({}).noUnknown()
+});
